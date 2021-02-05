061e232c8f6bba7019a1fcbd96cdea65d7412b45
gabomartin <75818899+gabomartin@users.noreply.github.com>
2021-02-05 08:00:43 -0300

    comentarios

--- test.s
+++ test.s
@@ -1,29 +1,3 @@
-         .data
-cat:  .asciiz "Introduzca nombre de categoria:\n"   
-obj:   .asciiz "Introduzca el nombre del objeto:\n"
-
-
-slist:  .word 0  # inicializado a null    (puntero al primer nodo)                                  <- punteros     
-cclist: .word 0  #                        (circular category list)(apunta a la lista de categorías)        <- punteros 
-wclist:          #                        (working category list)(la lista en la cual estamos trabajando)         <- punteros 
-prevcat:    # punteros o funciones?       
-nextcat:
-
-
-.text
-
-# void newnode(int number)
-
-newcat:
-delcat: 
-newobject: 
-delobject:
-
-               
-#//////////////// CODIGO DEL PROFE  
-#(hay que hacer multiples de estas para cada lista? seria una lista para las categorias q apunta a otra lista de cada item de la categoria...)
-#ademas esta pensada para solo enlazar una vez. habria q buscar la forma de linkear el END al START         
-
 # void newnode(int number) (((como le paso el argumento a $a0????)))
 newnode:     move $t0, $a0       # preserva arg 1
              li $v0, 9           # 9 es sbrk (allocate heap memory)$v0 contains address of allocated memory  
@@ -56,29 +30,3 @@ loop:        lw $a0, ($s0)
              addi $s1, $s1, -1
              bnez $s1, loop
              .end
-
-
-
-
-
-#smalloc:
-#             lw $t0, slist
-#             beqz $t0, sbrk
-#             move $v0, $t0
-#             lw $t0, 12($t0)
-#             sw $t0, slist
-#             jr $ra
-
-
-#sbrk:
-#             li $a0, 16          # node size fixed 4 words
-#             li $v0, 9
-#             syscall             # return node address in v0
-#             jr $ra
-#
-
-#sfree:
-#             la $t0, slist
-#             sw $t0, 12($a0)
-#             sw $a0, slist       # $a0 node address in unused list
-#             jr $ra
